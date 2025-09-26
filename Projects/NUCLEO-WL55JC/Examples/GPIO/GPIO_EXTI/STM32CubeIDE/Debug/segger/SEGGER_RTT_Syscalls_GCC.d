ecd8fc8395630c2120818383702f553a93fae0ef
Marina Holanda <marina.holanda@nepen.org.br>
2025-09-26 17:06:13 -0300

    Att 26.08

--- Projects/NUCLEO-WL55JC/Examples/GPIO/GPIO_EXTI/STM32CubeIDE/Debug/segger/SEGGER_RTT_Syscalls_GCC.d
+++
@@ -1,5 +0,0 @@
-segger/SEGGER_RTT_Syscalls_GCC.o: ../segger/SEGGER_RTT_Syscalls_GCC.c \
- C:/Users/Marina/IoG/iog-fw/fw/src/Middlewares/Third_Party/segger/SEGGER_RTT.h \
- C:/Users/Marina/IoG/iog-fw/fw/src/Middlewares/Third_Party/segger/SEGGER_RTT_Conf.h
-C:/Users/Marina/IoG/iog-fw/fw/src/Middlewares/Third_Party/segger/SEGGER_RTT.h:
-C:/Users/Marina/IoG/iog-fw/fw/src/Middlewares/Third_Party/segger/SEGGER_RTT_Conf.h:
